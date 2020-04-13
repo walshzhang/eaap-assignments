142634c32e9dc62d3502be53ad03b1d7428dfe47
walsh <walsh.zhang@hotmail.com>
2020-04-13 09:39:09 +0800

    test services work well.

--- assignments-management/src/main/java/iteach/eaap/assignments/management/Application.java
+++ assignments-management/src/main/java/iteach/eaap/assignments/management/Application.java
@@ -2,12 +2,14 @@ package iteach.eaap.assignments.management;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
-public class AssignmentManagementApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(iteach.eaap.assignments.management.AssignmentManagementApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
