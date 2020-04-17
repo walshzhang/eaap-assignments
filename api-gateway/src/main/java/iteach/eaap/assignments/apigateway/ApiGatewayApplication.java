a3f4dc28e5c9d361acad74afdd402a0ac92908df
walsh <walsh.zhang@hotmail.com>
2020-04-17 18:04:22 +0800

    add api gateway project

---
+++ api-gateway/src/main/java/iteach/eaap/assignments/apigateway/ApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package iteach.eaap.assignments.apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGatewayApplication.class, args);
+    }
+
+}
---
+++ api-gateway/src/test/java/iteach/eaap/assignments/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package iteach.eaap.assignments.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
